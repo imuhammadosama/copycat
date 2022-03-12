b41e654271ecc115fba55116fb03cd019831c530
Muhammad Osama <imuhammadosama@icloud.com>
2022-03-12 21:09:36 +0500

    Update

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import express from 'express';
 
 const app = express();
 
+require('dotenv').config();
+
+app.use(cors()); // We're telling express to use CORS
+app.use(express.json()); // we need to tell server to use json as well
+
 app.get('/', (req, res) => {
   res.send('API is working!');
 });
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('Server is listening to port 4000');
 });
